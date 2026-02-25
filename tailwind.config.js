00b7a756e7091209145bfb02cef99da4af994f2e
Optimus Prime <107649266+Hancock07@users.noreply.github.com>
2026-02-25 15:52:49 +0100

    Add files to start project
    Getting started

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html", "./components/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        dark: '#0F172A'
+      }
+    },
+  },
+  plugins: [],
+}
